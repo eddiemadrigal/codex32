8d021d619a2591737791dd2298f0a94bf714f710
Eddie Madrigal <edmadrigal@yahoo.com>
2016-05-31 20:14:55 -0700

    Create test.php

---
+++ test.php
@@ -0,0 +1,336 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+<head>
+  <title>Eddie Madrigal Full Stack Web Developer SEO SQL C# Java CMS</title>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta name="description" content="Full Stack Web Developer with SEO, SQL, C#, Java and CMS experience? Contact Eddie Madrigal at 916-286-9791. All code developed in the USA!">
+  <meta name="author" content="Eddie Madrigal">
+  <meta property="og:title" content="Eddie Madrigal Full Stack Web Developer SEO SQL C# Java CMS">
+  <meta property="og:type" content="article">
+  <meta property="og:url" content="http://codex32.com">
+  <meta property="og:image" content="http://codex32.com/images/matrix.jpg">
+  <meta name="twitter:card" content="summary">
+  <meta name="twitter:site" content="@edmadrigal">
+  <meta name="twitter:title" content="Eddie Madrigal Full Stack Web Developer SEO SQL C# Java CMS">
+  <meta name="twitter:description" content="Full Stack Web Developer with SEO, SQL, C#, Java and CMS experience? Contact Eddie Madrigal at 916-286-9791. All code developed in the USA!">
+  <link rel="publisher" href="https://plus.google.com/+EddieMadrigalWebDeveloper">
+  <!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <link rel="stylesheet" href="../assets/css/main.css" />
+  <!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+  <!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+  <link rel="stylesheet" type="text/css" href="../print.css" media="print">
+  <link rel="stylesheet" href="custom.css" type="text/css">
+
+  <script>
+    function showRSS(str, type) {
+      if (str.length == 0) {
+        document.getElementById("rssOutput").innerHTML = "";
+        return;
+      }
+      if (window.XMLHttpRequest) {
+        // code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp = new XMLHttpRequest();
+      } else { // code for IE6, IE5
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+      }
+      xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+          document.getElementById("rssOutput").innerHTML = xmlhttp.responseText;
+        }
+      }
+      xmlhttp.open("GET", "getRSS.php?t=" + type + "&q=" + str, true);
+      xmlhttp.send();
+    }
+
+  </script>
+</head>
+
+<body>
+
+  <!-- Sidebar -->
+  <section id="sidebar">
+    <div class="inner">
+      <nav>
+        <ul>
+          <li><a href="#intro">Welcome</a></li>
+          <li><a href="#feeds">News Feeds</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+      <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+      <!-- main ad -->
+      <ins class="adsbygoogle ad" data-ad-client="ca-pub-4730514140873041" data-ad-slot="4353626031" data-ad-format="auto"></ins>
+      <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+
+      </script>
+    </div>
+  </section>
+
+  <!-- Wrapper -->
+  <div id="wrapper">
+
+    <!-- Intro -->
+    <section id="intro" class="wrapper style1 fullscreen fade-up">
+      <div class="inner">
+        <h1 class="background-light">Eddie Madrigal Web Developer</h1>
+        <h3 class="background-light">Your Full Stack Web Developer with SEO, SQL, C#, Java and CMS Expertise!</h2>
+        <ul class="actions">
+          <li><a href="mailto:edmadrigal@yahoo.com" class="button scrolly background-light">Email Eddie Madrigal</a></li>
+        </ul>
+        <h2 class="background-light" style="margin-top: 60px; text-align: center;">Presents: ABC News Feeds <span id="spinningWheel"><img src="loading.gif" height="30px;"></span></h2>
+      </div>
+    </section>
+
+    <!-- two -->
+    <section id="feeds" class="wrapper style3 fade-up">
+    <div class="inner">
+      <h2>ABCNews.com RSS News Feeds</h2>
+
+      <p>
+      
+        <div class="row">
+          <div class="col-sm-3">
+          
+          <h2 style="font-size: 16px;">Category</h2>
+    <ul>
+      <li id="ABCNewsTopStories">Top Stories</li>
+      <li id="ABCWorldHeadlines">World Headlines</li>
+      <li id="ABCUSHeadlines">US Headlines</li>
+      <li id="ABCPoliticsHeadlines">Politics Headlines</li>
+      <li id="ABCTheBlotter">The Blotter from Brian Ross</li>
+    </ul>
+          
+          </div>
+          <div class="col-sm-9">
+          
+          <div id="rssOutput">
+      <h2 style="font-size: 12px;">Loading RSS Feeds ... <img src="loading.gif" height="30px;"></h2>
+    </div>
+          </div>
+      
+      </div>
+      
+      </p>
+
+    </div>
+  </section>
+
+    <section>
+      <div class="background">
+        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <!-- main ad -->
+        <ins class="adsbygoogle ad" data-ad-client="ca-pub-4730514140873041" data-ad-slot="4353626031" data-ad-format="auto"></ins>
+        <script>
+          (adsbygoogle = window.adsbygoogle || []).push({});
+
+        </script>
+      </div>
+    </section>
+
+    
+
+  
+
+  
+
+  <section id="contact" class="wrapper style1 fade-up">
+    <div class="inner">
+      <h2>Get in touch</h2>
+      <p>I'd love to hear from you. Please feel free to leave me a message and I will respond as quickly as I can. If you're looking for a web developer for your next project, email me your ideas and I will be happy to help put your site up!
+        <br>
+        <br>Thank you.</p>
+      <div class="split style1">
+        <section>
+          <form name="contact-form" action="mailSend.php" method="POST">
+            <div class="field half first">
+              <label for="name">Name</label>
+              <input type="text" name="sender_name" id="name" />
+            </div>
+            <div class="field half first">
+              <label for="name">Phone</label>
+              <input type="text" name="sender_phone" id="name" />
+            </div>
+            <div class="field">
+              <label for="email">Email</label>
+              <input type="text" name="sender_email" id="email" />
+            </div>
+            <div class="field">
+              <label for="message">Message</label>
+              <textarea name="sender_message" id="message" rows="5"></textarea>
+            </div>
+            <input type="submit" name="send_message" value="Send">
+            <div class="field">
+
+            </div>
+          </form>
+        </section>
+        <section>
+          <ul class="contact">
+            <li>
+              <h3>Mailing Address</h3>
+        <span>Eddie Madrigal<br>P.O. Box 582814<br />
+											Elk Grove, CA  95758<br />
+											USA</span>
+        </li>
+        <li>
+          <h3>Email</h3>
+          <a href="mailto:edmadrigal@yahoo.com">edmadrigal@yahoo.com</a>
+        </li>
+        <li>
+          <h3>Phone</h3>
+          <span><a href="tel:9162869791">(916) 286-9791</a></span>
+        </li>
+        <li>
+          <h3>Social</h3>
+          <ul class="icons">
+            <li><a href="https://twitter.com/edmadrigal" class="fa-twitter"><span class="label">Twitter</span></a></li>
+            <li><a href="http://www.facebook.com/eddiemadrigalwebdeveloper" class="fa-facebook"><span class="label">Facebook</span></a></li>
+            <li><a href="https://github.com/eddiemadrigal" class="fa-github"><span class="label">GitHub</span></a></li>
+            <li><a href="https://www.instagram.com/edmadrigal_" class="fa-instagram"><span class="label">Instagram</span></a></li>
+            <li><a href="https://www.linkedin.com/in/edmadrigal" class="fa-linkedin"><span class="label">LinkedIn</span></a></li>
+            <li><a href="https://www.youtube.com/user/eddiemadrigal" class="fa-youtube"><span class="label">YouTube</span></a></li>
+            <li><a href="https://plus.google.com/+EddieMadrigalWebDeveloper" class="fa-google-plus"><span class="label">GooglePLUS+</span></a></li>
+          </ul>
+        </li>
+        </ul>
+    </section>
+    </div>
+  </div>
+  </section>
+
+  <section>
+    <div class="background">
+      <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+      <!-- main ad -->
+      <ins class="adsbygoogle ad" data-ad-client="ca-pub-4730514140873041" data-ad-slot="4353626031" data-ad-format="auto"></ins>
+      <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+
+      </script>
+    </div>
+  </section>
+
+
+  </div>
+
+
+
+  <!-- Footer -->
+  <footer id="footer" class="wrapper style1-alt">
+    <div class="inner">
+      <hr>
+      <p class="footerStyle">&copy; 2016 Eddie Madrigal. All Rights Reserved.</p>
+    </div>
+  </footer>
+
+  <!-- Scripts -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <script src="../assets/js/jquery.scrollex.min.js"></script>
+  <script src="../assets/js/jquery.scrolly.min.js"></script>
+  <script src="../assets/js/skel.min.js"></script>
+  <script src="../assets/js/util.min.js"></script>
+  <!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+  <script src="../assets/js/main.min.js"></script>
+
+  <script>
+    (function(i, s, o, g, r, a, m) {
+      i['GoogleAnalyticsObject'] = r;
+      i[r] = i[r] || function() {
+        (i[r].q = i[r].q || []).push(arguments)
+      }, i[r].l = 1 * new Date();
+      a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0];
+      a.async = 1;
+      a.src = g;
+      m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+    ga('create', 'UA-54565010-1', 'auto');
+    ga('send', 'pageview');
+
+  </script>
+
+  <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+  <script>
+    $(document).ready(function() {
+
+      var loadRSSFeeds = function() {
+        $('#ABCNewsTopStories').css('cursor', 'pointer');
+        showRSS('ABCNewsTopStories', 'noDesc');
+      };
+
+      setTimeout(function() {
+        $('#spinningWheel').css('display', 'none');
+        setTimeout(function() {
+          window.location.hash = "feeds";
+          loadRSSFeeds();
+        }), 3000;
+      }, 7000);
+
+      var resetLinks = function() {
+        $('#ABCNewsTopStories').css('border-bottom', 'none');
+        $('#ABCWorldHeadlines').css('border-bottom', 'none');
+        $('#ABCUSHeadlines').css('border-bottom', 'none');
+        $('#ABCPoliticsHeadlines').css('border-bottom', 'none');
+        $('#ABCTheBlotter').css('border-bottom', 'none');
+      };
+
+      $('#ABCNewsTopStories').mouseover(function() {
+        resetLinks();
+        $('#ABCNewsTopStories').css('cursor', 'pointer');
+        $('#ABCNewsTopStories').css('border-bottom', '1px dotted #fff');
+        showRSS('ABCNewsTopStories', 'noDesc');
+      });
+      $('#ABCNewsTopStories').mouseout(function() {});
+
+      $('#ABCWorldHeadlines').mouseover(function() {
+        resetLinks();
+        $('#ABCWorldHeadlines').css('cursor', 'pointer');
+        $('#ABCWorldHeadlines').css('border-bottom', '1px dotted #fff');
+        showRSS('ABCWorldHeadlines', 'noDesc');
+      });
+      $('#ABCWorldHeadlines').mouseout(function() {});
+
+      $('#ABCUSHeadlines').mouseover(function() {
+        resetLinks();
+        $('#ABCUSHeadlines').css('cursor', 'pointer');
+        $('#ABCUSHeadlines').css('border-bottom', '1px dotted #fff');
+        showRSS('ABCUSHeadlines', 'noDesc');
+      });
+      $('#ABCUSHeadlines').mouseout(function() {});
+
+      $('#ABCPoliticsHeadlines').mouseover(function() {
+        resetLinks();
+        $('#ABCPoliticsHeadlines').css('cursor', 'pointer');
+        $('#ABCPoliticsHeadlines').css('border-bottom', '1px dotted #fff');
+        showRSS('ABCPoliticsHeadlines', 'noDesc');
+      });
+      $('#ABCPoliticsHeadlines').mouseout(function() {});
+
+      $('#ABCTheBlotter').mouseover(function() {
+        resetLinks();
+        $('#ABCTheBlotter').css('cursor', 'pointer');
+        $('#ABCTheBlotter').css('border-bottom', '1px dotted #fff');
+        showRSS('ABCTheBlotter', 'noDesc');
+      });
+      $('#ABCTheBlotter').mouseout(function() {});
+
+    });
+
+  </script>
+</body>
+
+</html>
+
+<!--
+	Hyperspace by HTML5 UP
+	html5up.net | @n33co
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
